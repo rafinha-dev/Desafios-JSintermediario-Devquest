8f87670006667e193ef4f1b2da23cfce9765beda
RafaelContact <contactrodrigues21@gmail.com>
2024-02-02 05:24:50 -0300

    fix: solution fix using event change

--- src/ex4/src/soluçãoPronta/index.js
+++ src/ex4/src/soluçãoPronta/index.js
@@ -19,9 +19,10 @@ let inputText = document.querySelectorAll(".input-text");
 // Passo 2 - Dar um jeito de percorrer cada input - vamos utilizar o laço de repetição forEach para percorrer cada input.
 inputText.forEach(input => {
     // Passo 3 - Quando o usuário preencher algo, precisamos disparar a ação para verificar se o input foi alterado.
-    input.addEventListener('change', () => {
+    input.addEventListener('change', (e) => {
         // Passo 4 - Realizar a verificação se o valor do input é diferente de vazio.
-        if (input.value !== "") {
+        let text = e.target
+        if (text.value !== "") {
             // Passo 5 - Se o input tiver alguma informação, adicionamos a classe de campo-preenchido que vai dar a borda verde ao input.
             input.classList.add("campo-preenchido")
         } else {
