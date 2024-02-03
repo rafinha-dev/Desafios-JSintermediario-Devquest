3d6242b68fb664f5afc8f37dc78d5961974aa90f
RafaelContact <contactrodrigues21@gmail.com>
2024-02-03 16:52:22 -0300

    feat: add ex2 a front end mode

--- src/ex2/src/index.js
+++ src/ex2/src/index.js
@@ -1,9 +1,28 @@
  let num = [65, 44, 12, 4]
+ let heigth = num.length 
+ let numbers = []
+ let hold = 0 
 
 // for(let i=0; i < num.length; i++){
 //     console.log(num[i] * 10 ) 
 // }
+const show_result = document.querySelector('#result')
+const btn = document.querySelector('.btn')
 
-num.forEach((num) => {
-    console.log(num * 10)
+const calc = ()=>{
+    num.forEach((element, i) => {
+    let mult = element * 10
+    numbers.push(mult)
+    if(i == 3){
+        console.log('feito')
+    }
+    
+    })
+}
+btn.addEventListener('click', ()=>{
+    if(hold == 0){   
+        calc()
+        show_result.innerHTML = `A multiplicação dos valores é [${numbers}]`
+    }
+    hold ++
 })
