39122853b7f91183abf6f7ad67e0dbc0af10e4cf
RafaelContact <contactrodrigues21@gmail.com>
2024-02-02 22:58:33 -0300

    refactor: identification classes and coments

--- src/ex6/src/assets/js/index.js
+++ src/ex6/src/assets/js/index.js
@@ -8,23 +8,22 @@ https://www.figma.com/file/zBKnYG9UNdUiIr8ClQTWSG/?node-id=3%3A3
 */ 
 
 let arrows = document.querySelectorAll('.acordeon')
-
+// quando usado queryselectorall não pode usar um listner nele, logo para que seja possível tem que usar o data. ou coloca-lo dentro de um foreach como elemento
 let colorArrows = document.querySelectorAll('.color-arrow')
-
-console.log(colorArrows)
 
 let i = [true, true, true]
 
 
 function response(num){
 
+    console.log(i[num])
     if(i[num] === false){
         hideAll()
 
     }else if(i[num] === true){
         
         hideAll()
-        arrows[num].classList.add("quest-response")
+        arrows[num].classList.add("show")
         i[num] = false
         arrows[num].classList.remove("hide")
         colorArrows[num].setAttribute('src', './src/assets/images/seta aberta.png')
@@ -33,11 +32,25 @@ function response(num){
 }
 
 
+
+// colorArrows.forEach( element =>{
+//     element.addEventListener("click", ()=>{
+//         element.classList.add('show')
+//         element.classList.remove('hide')
+//         console.log(element.value)
+
+        // element.classList.toggle("hide")
+        // o toggle é usado quando uma classe pode ser usada com classlist add e remove. Ele valida entre true e false encurtando o código e evitando ter que fazer todo esquema de add e remove, porém a mesma tem que funcionar pra adicionar e remover.
+//     })
+// })
+
+
+
 function hideAll(){
     for(let contador = 0; contador < arrows.length ; contador ++){
         // console.log(contador)
         i[contador] = true
-        arrows[contador].classList.remove("quest-response")
+        arrows[contador].classList.remove("show")
         arrows[contador].classList.add("hide")
         colorArrows[contador].setAttribute('src', './src/assets/images/Design sem nome (5) 1.svg')
     }
